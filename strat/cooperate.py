1d70acd66d40f5fecb557d4c26e1dee95b9e94f3
kaelan <git@bytemaker.io>
2023-09-18 01:56:06 +0200

    last commit

--- strat/cooperate.py
+++
@@ -1,5 +0,0 @@
-# NEPTUN1 NEPTUN2
-
-def decision(h1, h2):
-
-    return 0
--- strat/tit copy.py
+++
@@ -1,8 +0,0 @@
-def decision(h1,h2):
-    if len(h2)==0:
-        return 0
-    else:
-        if h2[-1]==1:
-            return 1
-        else:
-            return 0 
