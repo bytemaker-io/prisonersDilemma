ce8c88b12a6b7342d111a9e3328c4675870a3ab6
kaelan <git@bytemaker.io>
2023-09-18 01:56:40 +0200

    last commit

--- strat/IIONHK_Desicion copy 6.py
+++ strat/IIONHK_Desicion copy 6.py
@@ -1,12 +1,7 @@
 #Neptun: IIONHK
 def decision(h1, h2):
-    # Checking if this is the first round. If true, I will choose to cooperate.
     if len(h2) == 0:
         return 0
-    # If this is the last round,I will betray him 
-    if len(h2) >= 199:
-        return 1
-    #try to always cooperate with the opponent
     else:
        #tit for tat strategy
         if h2[-1] == 1:
